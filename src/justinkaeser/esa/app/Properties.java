bc1e9ca60d40c7d281e8e64abcf909b892acd926
Justin Kaeser <github@accounts.justinkaeser.com>
2015-03-19 00:36:33 +0100

    i think this is a permissions change

--- src/justinkaeser/esa/app/Properties.java
+++ src/justinkaeser/esa/app/Properties.java
@@ -4,9 +4,6 @@ import de.tudarmstadt.ukp.wikipedia.api.WikiConstants.Language;
 
 /**
  * This is like a properties file, but for programmers only. Use to set DB connection data.
- * 
- * @author Justin Kaeser
- *
  */
 public interface Properties {
 	
